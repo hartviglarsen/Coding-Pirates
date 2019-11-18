56d6951004b2925db821c36276563d9c70854575
hartviglarsen <hartvig@pm.me>
2019-11-18 15:09:48 +0100

    Add boundary collision

--- python/draw.py
+++ python/draw.py
@@ -28,6 +28,12 @@ window.onkey(turnLeft, "Left")
 window.onkey(turnRight, "Right")
 
 while True:
+    if character.xcor() > 300 or character.xcor() < -300:
+        character.right(180)
+
+    if character.ycor() > 300 or character.ycor() < -300:
+        character.left(180)
+
     character.forward(1)
 
 exitonclick()
