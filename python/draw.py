8e923f4054109c66163bf678640fa76d4eaa688f
hartviglarsen <hartvig@pm.me>
2019-11-18 14:55:14 +0100

    Add character with movement

--- python/draw.py
+++ python/draw.py
@@ -14,4 +14,20 @@ for side in range(4):
   pen.forward(600)
   pen.left(90)
 
+character = Turtle()
+character.color("blue")
+
+def turnLeft():
+    character.left(30)
+
+def turnRight():
+    character.right(30)
+
+window.listen()
+window.onkey(turnLeft, "Left")
+window.onkey(turnRight, "Right")
+
+while True:
+    character.forward(1)
+
 exitonclick()
